63463c3fed51ac3ceb489e60912ba100589453e6
Gaurav Vaidya <gaurav@renci.org>
2021-10-07 18:35:40 -0400

    Started trying to replicate the error.

---
+++ tests/test_issues/test_issue_new.py
@@ -0,0 +1,40 @@
+import unittest
+
+import yaml
+import io
+
+from tests.test_issues.input.kitchen_sink import Dataset
+from linkml_runtime.loaders import yaml_loader
+
+inp_yaml = """
+---
+companies:
+  - id: 201
+    name: 'Aperture Science'
+    ceo: 
+      - id: 1001
+        name: Cave Johnson
+        has employment history:
+          - ended at time: 2000-12-31
+            started at time: 1999-11-20
+          - ended at time: 2001-03-03
+            started at time: 2001-01-01
+      - id: 1002
+        name: Cave Johnson
+        has employment history:
+          - ended at time: 2000-12-31
+            started at time: 1999-11-20
+          - ended at time: 2001-03-03
+            started at time: 2001-01-01
+          
+  - id: 202
+    name: 'Goliath Corporation'
+"""
+
+
+class IssueNewTestCase(unittest.TestCase):
+    def test_import(self):
+        yaml_obj_list = yaml.load_all(io.StringIO(inp_yaml), Loader=yaml.FullLoader)
+        for yaml_obj in yaml_obj_list:
+            dataset = yaml_loader.load(yaml_obj, Dataset)
+            print(dataset['companies'])
