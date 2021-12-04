5200d417b4541a822409e6018cdf3ca563a3ad48
Bill Duncan <wdduncan@gmail.com>
2021-12-03 19:21:23 -0500

    check if the range of has_input is NamedThing within the python file

--- tests/test_issues/test_linkml_issue_497.py
+++ tests/test_issues/test_linkml_issue_497.py
@@ -1,3 +1,5 @@
+import re
+import logging
 from unittest import TestCase, main
 from linkml_runtime.utils.schemaview import SchemaView
 from tests.test_issues.environment import env
@@ -6,17 +8,27 @@ class TestLinkmlIssue497(TestCase):
    env = env
 
    def test_linkml_issue_497(self):
-       view = SchemaView(env.input_path('linkml_issue_497.yaml'))
-       biosample_processing = view.get_class('biosample processing')
-       omics_processing = view.get_class('omics processing')
+      
+      view = SchemaView(env.input_path('linkml_issue_497.yaml'))
+      biosample_processing = view.get_class('biosample processing')
+      omics_processing = view.get_class('omics processing')
 
-       has_input = view.get_slot('has input')
-       has_input_bp = view.induced_slot('has input', 'biosample processing')
-       has_input_op = view.induced_slot('has input', 'omics processing')
+      has_input = view.get_slot('has input')
+      has_input_bp = view.induced_slot('has input', 'biosample processing')
+      has_input_op = view.induced_slot('has input', 'omics processing')
 
-       assert has_input.range == 'named thing'
-       assert has_input_bp.range == 'biosample'
-       assert has_input_op.range == 'biosample'
+      # testing schemaview ranges
+      assert has_input.range == 'named thing'
+      assert has_input_bp.range == 'biosample'
+      assert has_input_op.range == 'biosample'
+
+      # test python file's has_input attribute
+      with open(env.input_path('linkml_issue_497.py')) as f:
+        for line in f:
+            if re.match(r'\s*has_input.*NamedThing.*', line):
+               logging.error('has_input range is NamedThing.', line.strip())
+               assert False
+
 
 if __name__ == "__main__":
-    main()
+   main()
