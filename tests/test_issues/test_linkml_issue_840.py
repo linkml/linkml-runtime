8fd46a9e1be68e62bc38de9a99b64fc3390eb093
Sujay Patil <sujaysanjeev.patil@gmail.com>
2022-06-17 15:02:33 -0700

    test case for linkml issue 840

---
+++ tests/test_issues/test_linkml_issue_840.py
@@ -0,0 +1,28 @@
+import unittest
+
+from linkml_runtime.utils.schemaview import SchemaView
+
+from tests.test_issues.environment import env
+
+SCHEMA = env.input_path("issue_840.yaml")
+
+
+class Issue840TestCase(unittest.TestCase):
+    env = env
+
+    def test_issue_840(self):
+        """schema_map attribute of SchemaView()"""
+        sv = SchemaView(SCHEMA)
+
+        # make call to imports_closure() method
+        # which returns all imports
+        sv.imports_closure()
+        actual_result = list(sv.schema_map.keys())
+
+        expected_result = ["issue-840", "issue-840-import"]
+
+        self.assertListEqual(actual_result, expected_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
