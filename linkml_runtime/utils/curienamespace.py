d7aee857153f410569bc98c121cd8e4e5e0309a0
hsolbrig <solbrig@jhu.edu>
2023-02-02 14:24:55 -0600

    Proposal for CurieNamespace with embedded catalog.
    It is my understanding that we are supposed to use the curies package, but it isn't clear how one
    would add maps incrementally (see: tests/test_utils/test_curienamespace.py#113 as an example). Any
    solution that passes test_curienamespace.py should do.

--- linkml_runtime/utils/curienamespace.py
+++ linkml_runtime/utils/curienamespace.py
@@ -1,16 +1,39 @@
-from typing import Optional, Union
+from logging import warning
+from typing import Optional, Union, Dict
 
 from rdflib import Namespace, URIRef
 
 
 class CurieNamespace(Namespace):
-    def __new__(cls, prefix: str, value: Union[str, URIRef]):
-        value = str(value)
-        try:
-            rt = str.__new__(cls, value)
-        except UnicodeDecodeError:
-            rt = str.__new__(cls, value, 'utf-8')
+    # We would prefer to use curies.Converter here, but there doesn't appear to be any way to build it incrementally
+    catalog: Dict[str, "CurieNamespace"] = dict()
+
+    @classmethod
+    def to_curie(cls, uri: Union[str, URIRef]) -> str:
+        uri = str(uri)
+        candidate_ns = ""
+        for prefix, ns in cls.catalog.items():
+            if uri.startswith(ns) and len(ns) > len(candidate_ns):
+                candidate_ns = ns
+        if candidate_ns:
+            return candidate_ns.curie(uri[len(candidate_ns):])
+        return None
+
+    @classmethod
+    def to_uri(cls, curie: str) -> Optional[URIRef]:
+        prefix, localname = curie.split(':', 1)
+        ns = CurieNamespace.catalog.get(prefix, None)
+        return ns[localname] if ns else None
+
+    def __new__(cls, prefix: str, ns: Union[str, bytes, URIRef]) -> "CurieNamespace":
+        rt = Namespace.__new__(cls, str(ns) if not isinstance(ns, bytes) else ns)
         rt.prefix = prefix
+        if prefix in CurieNamespace.catalog:
+            if CurieNamespace.catalog[prefix] != str(rt):
+                # prefix is bound to a different namespace
+                warning(f"Prefix: {prefix} already references {CurieNamespace.catalog[prefix]} - not updated to {rt}")
+        else:
+            CurieNamespace.catalog[prefix] = rt
         return rt
 
     def curie(self, reference: Optional[str] = '') -> str:
