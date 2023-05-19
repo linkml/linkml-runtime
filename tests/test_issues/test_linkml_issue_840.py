c6856c26457df02f18d70609266e0db5ba615cf4
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-05-19 16:32:44 -0700

    fix file path in test failure

--- tests/test_issues/test_linkml_issue_840.py
+++ tests/test_issues/test_linkml_issue_840.py
@@ -4,7 +4,7 @@ from linkml_runtime.utils.schemaview import SchemaView
 
 from tests.test_issues.environment import env
 
-SCHEMA = env.input_path("issue_840.yaml")
+SCHEMA = env.input_path("linkml_issue_840.yaml")
 
 
 class Issue840TestCase(unittest.TestCase):
