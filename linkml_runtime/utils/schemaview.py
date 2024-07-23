c22ca17161547378fde13f26133eece6b3ed2cfc
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-07-23 02:01:04 -0700

    or just do our own dict comprehension

--- linkml_runtime/utils/schemaview.py
+++ linkml_runtime/utils/schemaview.py
@@ -1343,7 +1343,7 @@ class SchemaView(object):
                              "or as a slot definition in the schema")
 
         # copy the slot, as it will be modified
-        induced_slot = SlotDefinition(**remove_empty_items(slot))
+        induced_slot = SlotDefinition(**{k:v for k,v in slot.__dict__.items() if v})
 
         # propagate inheritable_slots from ancestors
         if not slot_comes_from_attribute:
