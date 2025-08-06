fef0609c905afcddef5aadc91cf9d9116c9cf3a2
i alarmed alien <ialarmedalien@users.noreply.github.com>
2025-08-06 07:22:18 -0700

    Temporary insert for debugging purposes

--- tests/test_loaders_dumpers/test_loaders_pydantic.py
+++ tests/test_loaders_dumpers/test_loaders_pydantic.py
@@ -11,24 +11,30 @@ from tests.test_loaders_dumpers.environment import env
 from tests.test_loaders_dumpers.models.books_normalized_pydantic import BookSeries
 from tests.test_loaders_dumpers.models.kitchen_sink_pydantic import Dataset
 
-
+@pytest.mark.parametrize("format", list(LOADER.keys()))
 @pytest.mark.parametrize(
-    "filename,model,loader",
+    ("filename", "model"),
     [
-        ("book_series_lotr.yaml", BookSeries, yaml_loader),
-        ("book_series_lotr.json", BookSeries, json_loader),
-        ("kitchen_sink_normalized_inst_01.yaml", Dataset, yaml_loader),
-        ("kitchen_sink_normalized_inst_01.json", Dataset, json_loader),
+        ("book_series_lotr", BookSeries),
+        ("kitchen_sink_normalized_inst_01", Dataset),
     ],
 )
-def test_loader_basemodel(filename, model, loader):
-    name = Path(filename).stem
-    type = Path(filename).suffix.lstrip(".")
-    expected_yaml = env.expected_path("load", f"{name}_{type}.yaml")
-
+def test_loader_base_model(filename, model, format):
+    loader = LOADER[format]
+    expected_yaml = env.expected_path("load", f"{filename}_{format}.yaml")
     metadata = FileInfo()
 
-    python_obj: BaseModel = loader.load(filename, model, metadata=metadata, base_dir=env.indir)
+    python_obj: BaseModel = loader.load(f"{filename}.{format}", model, metadata=metadata, base_dir=env.indir)
+
+    # temporary insert for debugging
+    with open(expected_yaml) as expf:
+        expected = expf.read()
+    got = yaml_dumper.dumps(python_obj)
+    expected_trimmed = expected.replace("\r\n", "\n").strip()
+    got_trimmed = got.replace("\r\n", "\n").strip()
+    assert expected_trimmed == got_trimmed
+    # end insert
+
     assert env.eval_single_file(expected_yaml, yaml_dumper.dumps(python_obj))
 
     # Make sure metadata gets filled out properly using pathlib
@@ -37,4 +43,4 @@ def test_loader_basemodel(filename, model, loader):
     source_file = Path(metadata.source_file).resolve()
 
     assert Path("tests/test_loaders_dumpers/input") == base_path.relative_to(rel_path)
-    assert Path(f"tests/test_loaders_dumpers/input/{filename}") == source_file.relative_to(rel_path)
+    assert Path(f"tests/test_loaders_dumpers/input/{filename}.{format}") == source_file.relative_to(rel_path)
